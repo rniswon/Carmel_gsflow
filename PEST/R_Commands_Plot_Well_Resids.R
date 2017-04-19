c5b53f530bb5aa7dd17e9d96ca3d2d42dab83c5a
emorway-usgs <emorway@usgs.gov>
2017-04-18 17:20:26 -0700

    plot up well residuals from the first PEST run.

---
+++ PEST/R_Commands_Plot_Well_Resids.R
@@ -0,0 +1,14 @@
+prior <- read.table('D:/EDM_LT/GitHub/Carmel/Simulation_20161129_SteadyState/OUTPUT/hobs.out',header=FALSE,skip=1,col.names=c('sim','obs','name'))
+posterior <- read.table('D:/EDM_LT/GitHub/Carmel/PEST/Iteration_1_Back_From_Condor/OUTPUT/hobs.out',header=FALSE,skip=1,col.names=c('sim','obs','name'))
+
+prior$resid <- prior$sim - prior$obs
+posterior$resid <- posterior$sim - posterior$obs
+
+png('D:/EDM_LT/GitHub/Carmel/PEST/well_resids.png', res=120, width=700, height=700)
+hist(prior$resid, breaks=seq(-300,300,by=10), las=1, col='grey80', xlab='Residuals (m)', main='', ylim=c(0,100))
+hist(posterior$resid, add=TRUE, breaks=seq(-300,300,by=10), ang=45, den=25)
+legend("topright", c("Prior", "Posterior"), fill=c('grey80','black'), ang=c(NA,45), den=c(NA,25), bty="n", bg='white')
+dev.off()
+
+
+
