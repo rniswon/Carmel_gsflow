05c6a4de7d7d28a6ae56211032577c650fc880c0
GS\rniswon <rniswon@usgs.gov>
2017-04-28 17:24:31 -0700

    updated the transient simulation. working transient gsflow simulation is in Carmel.git\Simulation_20160812T131753

---
+++ Simulation_20160812T131753/INPUT/pest++/00_MF_Call_Array_Build.bat
@@ -0,0 +1,24 @@
+@echo off
+echo colrow=no           > settings.fig
+echo date=mm/dd/yyyy    >> settings.fig
+REM copy settings.fig .\Arrays  > nul
+REM echo  328  406    > t.GridSpecs.txt
+
+REM echo xx > t.crap
+REM copy t.crap .\Arrays  > nul
+
+REM __________________________________________________________________________________________________
+REM
+REM  
+
+REM                            %1    %2    %3    %4     %5      %6      %7          %8 
+REM                            --    --    --    --     --      --      --          -- 
+call 01_Sub.ArrayBuild.bat    HydKh  L_1   L_2   L_3   Lay_1   Lay_2   Lay_3      SurfK
+
+
+
+echo .
+echo *********************************************
+echo Finished Interpolating Pilot Points to Arrays
+echo *********************************************
+echo .
