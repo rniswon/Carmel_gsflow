fdc479e1918ca185f2036339dfc2d8e300ca58d6
emorway-usgs <emorway@usgs.gov>
2017-05-29 15:43:18 -0700

    Checking in results from PEST iteration #7
    Some results:
    
     Name                 Group          Measured         Modelled
     gage_out_h1          q_obs          322817.8         320122.4
     gwet                 gwet           100000.0         124745.7
     gwdischrg            rech           24074.30        -135.7000

--- PEST/R_Commands_Plot_Well_Resids.R
+++ PEST/R_Commands_Plot_Well_Resids.R
@@ -4,12 +4,15 @@ posterior2 <- read.table('D:/EDM_LT/GitHub/Carmel/PEST/Iteration_2_Back_From_Con
 posterior3 <- read.table('D:/EDM_LT/GitHub/Carmel/PEST/Iteration_3_Back_From_Condor/OUTPUT/hobs.out',header=FALSE,skip=1,col.names=c('sim','obs','name'))
 posterior4 <- read.table('D:/EDM_LT/GitHub/Carmel/PEST/Iteration_4_Back_From_Condor/OUTPUT/hobs.out',header=FALSE,skip=1,col.names=c('sim','obs','name'))
 
-posterior <- posterior4
+posterior6 <- read.table('D:/EDM_LT/GitHub/Carmel/PEST/Iteration_6_Back_From_Condor/OUTPUT/hobs.out',header=FALSE,skip=1,col.names=c('sim','obs','name'))
+posterior7 <- read.table('D:/EDM_LT/GitHub/Carmel/PEST/Iteration_7_Back_From_Condor/OUTPUT/hobs.out',header=FALSE,skip=1,col.names=c('sim','obs','name'))
+
+posterior <- posterior7
 
 prior$resid <- prior$sim - prior$obs
 posterior$resid <- posterior$sim - posterior$obs
 
-png('D:/EDM_LT/GitHub/Carmel/PEST/well_resids_4.png', res=120, width=700, height=700)
+png('D:/EDM_LT/GitHub/Carmel/PEST/well_resids_7.png', res=120, width=700, height=700)
 hist(prior$resid, breaks=seq(-300,300,by=10), las=1, col='grey80', xlab='Residuals (m)', main='', ylim=c(0,140))
 hist(posterior$resid, add=TRUE, breaks=seq(-300,300,by=10), ang=45, den=25)
 legend("topright", c("Prior", "Posterior"), fill=c('grey80','black'), ang=c(NA,45), den=c(NA,25), bty="n", bg='white')
