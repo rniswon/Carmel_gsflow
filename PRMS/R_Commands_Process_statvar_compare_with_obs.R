c5018e68b6ca7b85db9acd86d980962065268d22
GS\rniswon <rniswon@usgs.gov>
2018-06-18 16:25:04 -0700

    small changes to R code for comparing flows for PRMS only simulations

--- PRMS/R_Commands_Process_statvar_compare_with_obs.R
+++ PRMS/R_Commands_Process_statvar_compare_with_obs.R
@@ -2,8 +2,8 @@
 library(hydroGOF)
 library(gdata)
 
-setwd('D:/EDM_LT/github/Carmel/PRMS')
-#setwd('C:/Users/rniswon/Documents/Data/Git/carmel.git/PRMS')
+#setwd('D:/EDM_LT/github/Carmel/PRMS')
+setwd('C:/Users/rniswon/Documents/Data/Git/carmel.git/PRMS')
 
 # Add capability for appending plot version # to end of plot file names
 # Increment this value with each run of the script to preserve old output for comparing with new output
